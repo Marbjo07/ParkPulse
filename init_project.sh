b074f679713a481dafdaa313dafbc599353f990b
Marius Bjørhei <marius.bjorhei@gmail.com>
2024-08-16 15:12:02 +0200

    Fix: Use SSH Key to Access Git Repos

--- init_project.sh
+++ init_project.sh
@@ -10,17 +10,22 @@ mkdir -p parkpulse-api
 mkdir -p parkpulse-web
 mkdir -p parkpulse-accessmanager
 
-# Clone the repositories into their respective directories if they haven't been cloned already
+# Configure SSH for Git
+mkdir -p ~/.ssh
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+# Clone the repositories using SSH
 if [ ! -d "parkpulse-api/.git" ]; then
-    git clone https://github.com/Marbjo07/ParkPulse-API parkpulse-api
+    git clone git@github.com:Marbjo07/ParkPulse-API.git parkpulse-api
 fi
 
 if [ ! -d "parkpulse-web/.git" ]; then
-    git clone https://github.com/Marbjo07/ParkPulse-website parkpulse-web
+    git clone git@github.com:Marbjo07/ParkPulse-website.git parkpulse-web
 fi
 
 if [ ! -d "parkpulse-accessmanager/.git" ]; then
-    git clone https://github.com/Marbjo07/ParkPulse-AccessManager parkpulse-accessmanager
+    git clone git@github.com:Marbjo07/ParkPulse-AccessManager.git parkpulse-accessmanager
 fi
 
 # Navigate to each directory and perform a git pull
